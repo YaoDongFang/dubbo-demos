d523c91ade8c22a6d1920dd44df5814fec9c24f0
YaoDongFang <761042759@qq.com>
2020-08-18 22:18:16 +0800

    创建项目

---
+++ dubbo-demo-echo/dubbo-samples-echo-api/src/main/java/com/alibaba/dubbo/demo/echo/api/EchoService.java
@@ -0,0 +1,14 @@
+package com.alibaba.dubbo.demo.echo.api;
+
+/**
+ * EchoService
+ *
+ * @author YaoDF
+ * Description:
+ * Created in: 2020-08-18 22:16
+ * Modified by:
+ */
+public interface EchoService {
+
+    String echo(String message);
+}
